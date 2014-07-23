9d071421b6723a80ad6fe9e3dc3063519c0d70da
Goutte <antoine.goutenoir@gmail.com>
2014-07-23 00:12:35 +0000

    Add exhaustive(-to-be) test-case for shader preprocessor.

--- lib/src/layer/DidiLayer.dart
+++ lib/src/layer/DidiLayer.dart
@@ -2,16 +2,36 @@ part of dax;
 
 
 /**
- * Basic color shader, no fancy effects.
- * The default color is pure white, with full opacity.
- *
- * I wonder if this is the same as Ambient Color ?
+ * Didi's joy
  */
 class DidiLayer extends MaterialLayer {
 
   String get glslFragment => """
 uniform vec3 uColor;
 uniform float uAlpha;
+
+
+vec3 rainbow(float x)
+{
+	/*
+		Target colors
+		=============
+
+		L  x   color
+		0  0.0 vec4(1.0, 0.0, 0.0, 1.0);
+		1  0.2 vec4(1.0, 0.5, 0.0, 1.0);
+		2  0.4 vec4(1.0, 1.0, 0.0, 1.0);
+		3  0.6 vec4(0.0, 0.5, 0.0, 1.0);
+		4  0.8 vec4(0.0, 0.0, 1.0, 1.0);
+		5  1.0 vec4(0.5, 0.0, 0.5, 1.0);
+	*/
+
+	float level = floor(x * 6.0);
+	float r = float(level <= 2.0) + float(level > 4.0) * 0.5;
+	float g = max(1.0 - abs(level - 2.0) * 0.5, 0.0);
+	float b = (1.0 - (level - 4.0) * 0.5) * float(level >= 4.0);
+	return vec3(r, g, b);
+}
 
 void main(void) {
     gl_FragColor = vec4(uColor, uAlpha);
--- test/core/Material.dart
+++ test/core/Material.dart
@@ -15,25 +15,41 @@ class TestMaterial extends Material {
 
 }
 
-
+/**
+ * Showcases the features of dax's glsl parser.
+ * Supports :
+ * - mangling of :
+ *   - main()
+ *   - attribute
+ *   - uniform
+ *   - varying
+ * -
+ * Todo support :
+ * - varying int a, b, c;
+ * - shared attribute
+ * - shared uniform
+ * - shared varying
+ */
 class TestLayerA extends MaterialLayer {
   String get glslFragment => """
-precision mediump float;
+varying vec3 vPos;
+
 void main(void) {}
   """;
   String get glslVertex => """
 attribute vec3 aTest;
 uniform mat4 uTest;
+varying vec3 vPos;
 
 void main(void) {
     gl_Position = uTest * vec4(aTest, 1.0);
+    vPos = aTest.xyz;
 }
   """;
 }
 
 class TestLayerB extends MaterialLayer {
   String get glslFragment => """
-precision mediump float;
 uniform vec3 uGlobalColor;
 
 void main(void) {
@@ -55,14 +71,51 @@ main() {
 
   test('merges layers GLSL', () {
     Material material = new TestMaterial();
-    String expectedFragment = """
+    String expectedVertex = """
+attribute vec3 TestLayerA_aTest;
+uniform mat4 TestLayerA_uTest;
+uniform mat4 TestLayerB_uTest;
+varying vec3 TestLayerA_vPos;
 
+void main_TestLayerA(void) {
+    gl_Position = TestLayerA_uTest * vec4(TestLayerA_aTest, 1.0);
+    TestLayerA_vPos = TestLayerA_aTest.xyz;
+}
+void main_TestLayerB(void) {
+    // nothing
+}
+
+void main(void) {
+main_TestLayerA();
+main_TestLayerB();
+}
+    """;
+    String expectedFragment = """
+precision mediump float;
+precision mediump int;
+
+uniform vec3 TestLayerB_uGlobalColor;
+varying vec3 TestLayerA_vPos;
+
+void main_TestLayerA(void) {}
+void main_TestLayerB(void) {
+    gl_FragColor = vec4(TestLayerB_uGlobalColor, 1.0);
+}
+
+void main(void) {
+main_TestLayerA();
+main_TestLayerB();
+}
     """;
     expect(material.fragment is Shader, isTrue);
     expect(material.vertex is Shader, isTrue);
 
     print(material.vertex);
     print(material.fragment);
+
+    expect(material.vertex.toString(), equalsIgnoringWhitespace(expectedVertex));
+    expect(material.fragment.toString(), equalsIgnoringWhitespace(expectedFragment));
+
   });
 
 
