6277b4c1d746c5ff56cd82eb522567db0efc1dc3
Goutte <antoine.goutenoir@gmail.com>
2014-08-02 14:37:35 +0000

    Add [alpha] parameter to ColorLayer.

--- lib/src/layer/ColorLayer.dart
+++ lib/src/layer/ColorLayer.dart
@@ -3,9 +3,24 @@ part of dax;
 
 /**
  * Basic color shader, no fancy effects.
+ * Alpha will not work unless you enable GL_BLEND. And even then, it will be
+ * highly dependent on the scene graph drawing order, which is always the same
+ * right now. (and not guaranteed either, feel free to fix this)
  * The default [color] is poupou (#FF3399), with full opacity.
  */
 class ColorLayer extends MaterialLayer {
+
+  Vector3 color = new Vector3(1.0, 0.2, 0.6);
+  double  alpha = 1.0;
+
+  ColorLayer([Vector3 color, double alpha]) : super() {
+    if (color != null) {
+      this.color.setFrom(color);
+    }
+    if (alpha != null) {
+      this.alpha = alpha;
+    }
+  }
 
   String get glslFragment => """
 uniform vec3 uColor;
@@ -18,16 +33,8 @@ void main(void) {
 
   Map<String, dynamic> onSetup(World world, Model model, Renderer renderer) {
     return {
-      'uColor': color,
-      'uAlpha': 1.0,
+        'uColor': color,
+        'uAlpha': alpha,
     };
-  }
-
-  Vector3 color = new Vector3(1.0, 0.2, 0.6); // poupou = #FF3399
-
-  ColorLayer([Vector3 color]) : super() {
-    if (color != null) {
-      this.color.setFrom(color);
-    }
   }
 }
