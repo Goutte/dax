6cb4757ca460394a03fbd897082db226ea6b5e6e
Goutte <antoine.goutenoir@gmail.com>
2014-10-08 15:18:49 +0000

    Add comments about the shared varyings FRAGMENT_XXX_POSITION

--- lib/src/layer/PositionLayer.dart
+++ lib/src/layer/PositionLayer.dart
@@ -2,15 +2,22 @@ part of dax;
 
 
 /**
- * This material layer takes care of `gl_Position`, and sets VERTEX_POSITION
+ * This material layer takes care of `gl_Position`, and sets some variables
  * to be shared amongst all future layers of the material.
- * It should be part of all materials, as no other shader will take care of
- * gl_Position, unless you roll your own that does.
+ * It should be part of all materials, as no other shader layer will take care
+ * of gl_Position, unless you roll your own that does.
  *
- * VERTEX_POSITION holds the raw coordinates of the vertices of the mesh,
+ * VERTEX_POSITION holds the raw/local coordinates of the vertices of the mesh,
  * so they're only influenced by the mesh constructor. (`size` option, etc.)
  *
- * The matrices will also be shared eventually.
+ * FRAGMENT_LOCAL_POSITION is a varying for to the fragment shader, and holds
+ * the position of the fragment in its local mesh.
+ *
+ * FRAGMENT_WORLD_POSITION is a varying for to the fragment shader, and hold the
+ * position of the fragment after its model matrix multiplication, so it's in
+ * world space.
+ *
+ * The matrices will also probably be shared eventually.
  */
 class PositionLayer extends MaterialLayer {
 
@@ -21,8 +28,11 @@ uniform mat4 uMMatrix;
 uniform mat4 uVMatrix;
 uniform mat4 uPMatrix;
 
+// Holds the fragment position in the local mesh
 shared varying vec3 FRAGMENT_LOCAL_POSITION;
+// Holds the fragment position in the world
 shared varying vec3 FRAGMENT_WORLD_POSITION;
+
 
 void main(void) {
     vec4 worldPosition = uMMatrix * vec4(VERTEX_POSITION, 1.0);
@@ -47,7 +57,7 @@ shared varying vec3 FRAGMENT_LOCAL_POSITION;
     return {
         'VERTEX_POSITION': model.mesh.vertices,
         'uMMatrix': model.matrix,
-        // todo: compute only if the camera moved
+        // todo: precompute in Dart the multiplication, only if the camera moved
         'uPMatrix': world.camera.perspectiveMatrix,
         'uVMatrix': world.camera.viewMatrix,
     };
